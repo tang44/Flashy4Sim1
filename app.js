293183f3e9b4c74d746f4b85a8e9349795ac1285
tang44 <143195729+tang44@users.noreply.github.com>
2026-09-08 16:15:25 -0400

    Update data

--- app.js
+++ app.js
@@ -9,7 +9,7 @@
    CONFIGURATION
 ============================================================ */
 
-const EXCEL_FILE = "data/WM_level1_all_with_HSK.xlsx";
+const EXCEL_FILE = "WM_level1_all_with_HSK.xlsx";
 
 //WM_level1_all_with_HSK.xlsx
 
